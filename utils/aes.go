44cd24af97e50fe3f1ecd6f809a61e9b30f8b301
Lorenzo Stflorant <40050834+liushuojia@users.noreply.github.com>
2024-07-18 11:48:27 +0800

    Update aes.go
    change func aes cbc

--- utils/aes.go
+++ utils/aes.go
@@ -36,22 +36,21 @@ import (
 */
 
 // =================== CBC ======================
-
-func AesEncryptCBC(origData []byte, key []byte) (encrypted []byte) {
+func AesEncryptCBC(origData, key, iv []byte) (encrypted []byte) {
 	// 分组秘钥
 	// NewCipher该函数限制了输入k的长度必须为16, 24或者32
 	block, _ := aes.NewCipher(key)
 	blockSize := block.BlockSize()                              // 获取秘钥块的长度
 	origData = pkcs5Padding(origData, blockSize)                // 补全码
-	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize]) // 加密模式
+	blockMode := cipher.NewCBCEncrypter(block, iv) 		    // 加密模式
 	encrypted = make([]byte, len(origData))                     // 创建数组
 	blockMode.CryptBlocks(encrypted, origData)                  // 加密
 	return encrypted
 }
-func AesDecryptCBC(encrypted []byte, key []byte) (decrypted []byte) {
+func AesDecryptCBC(encrypted, key, iv []byte) (decrypted []byte) {
 	block, _ := aes.NewCipher(key)                              // 分组秘钥
 	blockSize := block.BlockSize()                              // 获取秘钥块的长度
-	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize]) // 加密模式
+	blockMode := cipher.NewCBCDecrypter(block, iv) 		    // 加密模式
 	decrypted = make([]byte, len(encrypted))                    // 创建数组
 	blockMode.CryptBlocks(decrypted, encrypted)                 // 解密
 	decrypted = pkcs5UnPadding(decrypted)                       // 去除补全码
