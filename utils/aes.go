f1d04a04869d429f93648a69a5d909cb81156a3c
holiyliu <holoyliu@futunn.com>
2024-08-08 22:40:53 +0800

    change

--- utils/aes.go
+++ utils/aes.go
@@ -40,20 +40,20 @@ func AesEncryptCBC(origData, key, iv []byte) (encrypted []byte) {
 	// 分组秘钥
 	// NewCipher该函数限制了输入k的长度必须为16, 24或者32
 	block, _ := aes.NewCipher(key)
-	blockSize := block.BlockSize()                              // 获取秘钥块的长度
-	origData = pkcs5Padding(origData, blockSize)                // 补全码
-	blockMode := cipher.NewCBCEncrypter(block, iv) 		    // 加密模式
-	encrypted = make([]byte, len(origData))                     // 创建数组
-	blockMode.CryptBlocks(encrypted, origData)                  // 加密
+	blockSize := block.BlockSize()                 // 获取秘钥块的长度
+	origData = pkcs5Padding(origData, blockSize)   // 补全码
+	blockMode := cipher.NewCBCEncrypter(block, iv) // 加密模式
+	encrypted = make([]byte, len(origData))        // 创建数组
+	blockMode.CryptBlocks(encrypted, origData)     // 加密
 	return encrypted
 }
 func AesDecryptCBC(encrypted, key, iv []byte) (decrypted []byte) {
-	block, _ := aes.NewCipher(key)                              // 分组秘钥
-	blockSize := block.BlockSize()                              // 获取秘钥块的长度
-	blockMode := cipher.NewCBCDecrypter(block, iv) 		    // 加密模式
-	decrypted = make([]byte, len(encrypted))                    // 创建数组
-	blockMode.CryptBlocks(decrypted, encrypted)                 // 解密
-	decrypted = pkcs5UnPadding(decrypted)                       // 去除补全码
+	block, _ := aes.NewCipher(key) // 分组秘钥
+	//blockSize := block.BlockSize()                              // 获取秘钥块的长度
+	blockMode := cipher.NewCBCDecrypter(block, iv) // 加密模式
+	decrypted = make([]byte, len(encrypted))       // 创建数组
+	blockMode.CryptBlocks(decrypted, encrypted)    // 解密
+	decrypted = pkcs5UnPadding(decrypted)          // 去除补全码
 	return decrypted
 }
 func pkcs5Padding(ciphertext []byte, blockSize int) []byte {
